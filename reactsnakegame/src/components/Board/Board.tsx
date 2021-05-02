4689058b51e4227b08ca71664b429b18754f996f
sailakshmy <sailakshmy94@gmail.com>
2021-05-02 17:50:35 +0530

    Commit-3: Created a Board of size 10x10, Added the styles for the Board, row, cell, snakecell and food cell, Removed the React Logo

--- reactsnakegame/src/components/Board/Board.tsx
+++ reactsnakegame/src/components/Board/Board.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
+import './Board.css';
 
 const Board = () => {
+  const BOARD_SIZE = 10;
+  const createBoard=(BOARD_SIZE:number)=>{
+    const board=[];
+    let counter=1;
+    for(let row=0;row<BOARD_SIZE;row++){
+        const currentRow = [];
+        for(let col=0;col<BOARD_SIZE;col++){
+          currentRow.push(counter++);
+        }
+        board.push(currentRow);
+    }
+    return board;
+  }
+
+  const [board,setBoard] = useState(createBoard(BOARD_SIZE));
   return (
-    <div>
-    </div>
+    <div className="board">
+        {board.map((row, rowIdx) => (
+          <div key={rowIdx} className="row">
+            {row.map((cellValue, cellIdx) => {
+              // const className = getCellClassName(
+              //   cellValue,
+              //   foodCell,
+              //   foodShouldReverseDirection,
+              //   snakeCells,
+              // );
+              return <div key={cellIdx} className={`cell ${true?'snake-cell':''}`}></div>;
+            })}
+          </div>
+        ))}
+      </div>
   )
 }
 
-export default Board
+export default Board;
